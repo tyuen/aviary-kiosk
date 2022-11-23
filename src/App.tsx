8be4840a4b12808865c9379e392d49f8b8d58e3f
Terry <tyuen1@gmail.com>
2022-11-23 16:52:58 +0800

    many birds; localization

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+import { useState, useRef, useEffect } from "react";
 // @ts-ignore
 import styles from "./App.module.css";
 import SwipeIntro from "components/SwipeIntro";
 import Carousel from "components/Carousel";
-import { useState, useRef, useEffect } from "react";
+import Button from "components/Button";
 
 const sounds = {
   menu: new Audio("./assets/317273__dpoggioli.wav"),
@@ -11,7 +12,13 @@ const sounds = {
 sounds.menu.volume = 0.5;
 
 function App() {
-  const [data, setData] = useState({ list: [] });
+  const [data, setData] = useState({
+    title: {},
+    intro: {},
+    back: {},
+    list: [],
+  });
+  const [lang, setLang] = useState("z");
 
   useEffect(() => {
     //https://www.wetlandpark.gov.hk/tc/biodiversity/wildlife-watching-calendar/birds
@@ -74,7 +81,7 @@ function App() {
   return (
     <div className="flex flex-col items-stretch h-full overflow-hidden text-white">
       <header
-        className="flex justify-between pt-1 bg-stone-100"
+        className="flex items-center justify-between pt-1 bg-white"
         style={{ boxShadow: "0 1px 10px rgba(0 0 0/.4)" }}
       >
         <img
@@ -83,33 +90,43 @@ function App() {
           alt=""
           onClick={() => changePage(-1)}
         />
-        <img src="assets/hksar25.png" className="h-20 mr-2" alt="" />
+        <img src="assets/hksar25.png" className="my-3 mr-2 h-14" alt="" />
       </header>
 
       <article ref={bookRef} className="relative z-0 flex-1 overflow-hidden">
         <article className="absolute inset-0 flex flex-col items-center justify-center w-full h-full p-0 m-0 overflow-hidden transition-all duration-500 scale-150 opacity-0">
-          <h1 className="mb-8 text-4xl">香港公園觀鳥園</h1>
+          <h1 className="mb-4 text-3xl text-center">
+            <div>{data.title[lang]}</div>
+          </h1>
 
-          <nav className="grid grid-cols-4 gap-[3vmin] mb-5">
+          <div className="mb-4 justify-evenly mx-[10vw]">
+            {data.intro[lang]}
+          </div>
+
+          <nav className="grid grid-cols-4 gap-[3vmin]">
             {data.list.map((m, i) => (
               <button
                 key={i}
-                className="z-0 relative flex justify-center items-center text-center border-4 border-stone-700 rounded-lg w-[16vmin] h-[16vmin] text-lg text-white transition-transform overflow-hidden active:scale-90 bg-black"
+                className="z-0 relative flex justify-center items-end text-center border-4 border-stone-700 rounded-lg w-[16vmin] h-[16vmin] text-lg text-white transition-transform overflow-hidden active:scale-90 bg-black leading-tight p-2"
                 style={{
                   textShadow: "0 0 4px #000, 0 0 10px #000",
                 }}
                 onClick={e => changePage(i)}
               >
                 <img
-                  src={"./data/" + m.img}
-                  className="absolute object-cover w-full min-h-full -z-10 opacity-70"
+                  src={"./data/" + m.img[0]}
+                  className="absolute inset-0 object-cover w-full min-h-full -z-10 opacity-70"
                   style={{ imageRendering: "crisp-edges" }}
                   alt=""
                 />
-                {m.h1}
+                {m.h1[lang]}
               </button>
             ))}
           </nav>
+          <div className="flex justify-center gap-2 mt-4">
+            <Button onClick={() => setLang("z")}>中文</Button>
+            <Button onClick={() => setLang("e")}>Eng</Button>
+          </div>
         </article>
 
         <article className="absolute inset-0 flex flex-col items-center w-full h-full p-0 m-0 overflow-hidden transition-all duration-500 translate-y-full opacity-0">
@@ -130,24 +147,36 @@ function App() {
                     "linear-gradient(45deg,#444,#333,#222,#444,#222)",
                 }}
               >
-                <header className="text-3xl text-center">{info.h1}</header>
+                <header className="flex items-center justify-between text-3xl text-center">
+                  <div className="w-[6ch]"></div>
+                  <div>{info.h1[lang]}</div>
+                  <div className="w-[6ch] text-right">
+                    <Button onClick={() => setLang(lang === "z" ? "e" : "z")}>
+                      {lang === "z" ? "Eng" : "中文"}
+                    </Button>
+                  </div>
+                </header>
                 <table cellSpacing={0} cellPadding={10} border={0}>
                   <tbody>
                     {info.dl.map(([h2, text], i) => (
                       <tr key={i}>
-                        <td className="font-bold whitespace-nowrap">{h2}</td>
-                        <td>{text}</td>
+                        <td className="font-bold align-top whitespace-nowrap">
+                          {h2[lang]}
+                        </td>
+                        <td className="align-top">{text[lang]}</td>
                       </tr>
                     ))}
                   </tbody>
                 </table>
-                <div className="relative grow min-h-[25vh]">
-                  <img
-                    src={"./data/" + info.img}
-                    className="absolute object-scale-down w-full h-full min-h-0"
-                    alt=""
-                  />
-                </div>
+                {info.img.map(src => (
+                  <div key={src} className="relative grow min-h-[25vh] mt-4">
+                    <img
+                      src={"./data/" + src}
+                      className="absolute object-scale-down w-full h-full min-h-0"
+                      alt=""
+                    />
+                  </div>
+                ))}
                 {info.audio ? (
                   <audio
                     ref={n => (mediaRef.current[i] = n)}
@@ -164,18 +193,9 @@ function App() {
             ))}
           </Carousel>
           <SwipeIntro className="mb-3 -mt-10" />
-          <button
-            className="min-h-0 px-4 py-1 mb-10 text-lg text-white border rounded-full border-stone-400 bg-stone-600 shrink-0 active:scale-90"
-            style={{
-              boxShadow:
-                "1px 1px 20px rgba(0 0 0/.5), inset 1px 1px 53x rgba(0 0 0/.5)",
-              backgroundImage:
-                "linear-gradient(45deg,#444,#333,#222,#444,#222)",
-            }}
-            onClick={() => changePage(-1)}
-          >
-            &larr; 返回主頁
-          </button>
+          <Button onClick={() => changePage(-1)}>
+            &larr; {data.back[lang] || ""}
+          </Button>
         </article>
       </article>
     </div>
---
+++ src/components/Button.tsx
@@ -0,0 +1,15 @@
+export default function Button({ children, onClick, className = "" }) {
+  return (
+    <button
+      className="min-h-0 px-4 py-1 mb-10 text-lg text-white border rounded-full border-stone-400 bg-stone-600 shrink-0 active:scale-90"
+      style={{
+        boxShadow:
+          "1px 1px 20px rgba(0 0 0/.5), inset 1px 1px 53x rgba(0 0 0/.5)",
+        backgroundImage: "linear-gradient(45deg,#444,#333,#222,#444,#222)",
+      }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
